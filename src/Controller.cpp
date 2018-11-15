9489dd4ad5d5e5110eefcb50c07a4977b4b3854a
BuildTools <unconfigured@null.spigotmc.org>
2018-11-15 16:14:53 +0100

    Implemented a few fonctions

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -30,14 +30,63 @@ Controller::Controller(double P, double I, double D){
   // Asserv en vitesse => commande de base à 0, robot immobile
   leftMotor = new PID(P, I, D, 0);
   rightMotor = new PID(P, I, D, 0);
+
+  lastUpdate=micros();
+  lastPosY = 0.0;
+  lastPosX = 0.0;
+
+  checkpointAmount = 0; //Car la liste checkpoints[] n'est pas initialisée
+  currentCheckpoint = 1; //Par défaut, il est considéré que nous sommes arrivés à destination.... oui, sérieusement x)
 }
 
-void Controller::setTarget(std::vector<Point> checkpoints, double targetedAngle){
+void Controller::setTarget(Point[] checkpoints, int checkpointAmount, double targetedAngle){
   this->checkpoints = checkpoints;
   this->targetedAngle = targetedAngle;
+
+  this->checkpointAmount = checkpointAmount;
+  currentCheckpoint = 0;
+  isRotating = true;
 }
 
+double targetedAngle, distanceToNode, currentSpeed, idealSpeed;
 void Controller::update(double posX, double posY, double currentAngle){
+
+  currentSpeed = sqrt(pow(lastPosX-posX,2)+pow(lastPosY-posY, 2))/(micros()-lastUpdate);
+  lastUpdate=micros();
+  lastPosY = posY;
+  lastPosX = posX;
+
+  targetSelection:;
+
+  // Si on est arrivé à l'objectif, on ne fait plus rien
+  if(currentCheckpoint>checkpointAmount){
+    return;
+  }
+
+  // calcul de la distance et de l'angle à l'objectif:
+  distanceToNode=sqrt(pow(checkpoints[currentCheckpoint].x-x,2)+pow(checkpoints[currentCheckpoint].y-y,2));
+  targetedAngle=atan2(ty-y,tx-x);
+
+  if(distanceToNode<PRECISION){
+    currentCheckpoint++;
+    isRotating = true;
+    goto targetSelection;
+  }
+
+  if(isRotating){
+    
+  }else{
+    // Calcul de la consigne en vitesse:
+    idealSpeed=distanceToNode/MAX_ACCELERATION;
+    if(abs(currentSpeed-idealSpeed)<MAX_ACCELERATION){
+      targetedSpeed = idealSpeed;
+    }else if(currentSpeed<idealSpeed){
+      targetedSpeed += MAX_ACCELERATION;
+    }else{
+      targetedSpeed -= MAX_ACCELERATION;
+    }
+  }
+
   leftMotor.update(currentSpeedL);
   rightMotor.update(currentSpeedR);
 }
--- src/Controller.hpp
+++ src/Controller.hpp
@@ -5,13 +5,18 @@
 
 #include <vector>
 
+#define MAX_ACCELERATION 0.2
+#define PRECISION 10
+
+typedef struct {double x,y;} Point;
+
 class Controller{
   public:
     Controller(double P, double I, double D);
+
     /** Commandes de création de trajectoire
      * En gros l'idée c'est de décomposer nos trajectoires en clothoïdes.
      */
-     typedef struct {double x,y;} Point;
      void setTarget(std::vector<Point> checkpoints, double targetedAngle);
 
     /**
@@ -24,6 +29,15 @@ class Controller{
     // Définition de la trajectoire
     std::vector<Point> checkpoints;
     double targetedAngle;
+    double targetedSpeed;
+
+    long lastUpdate;
+    double lastPosX, lastPosY;
+
+    int currentCheckpoint;
+    int checkpointAmount;
+
+    bool isRotating;
 
     // Sous-éléments du controller & autre
     PID leftMotor, rightMotor;
